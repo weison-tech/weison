ba796854182b744dd37da2b8dd25bbed17152915
weison <xiaomalover@gmail.com>
2015-04-04 19:05:53 +0800

    update

--- api/versions/v1/controllers/UserController.php
+++ api/versions/v1/controllers/UserController.php
@@ -4,6 +4,8 @@ namespace api\versions\v1\controllers;
 use common\models\LoginForm;
 use yii\filters\RateLimiter;
 use yii\rest\ActiveController;
+use api\common\models\SignupForm;
+use yii;
 
 class UserController extends ActiveController
 {
@@ -21,7 +23,7 @@ class UserController extends ActiveController
         $behaviors['verbs'] = [
             'class' => \yii\filters\VerbFilter::className(),
             'actions' => [
-                'login' => ['POST', 'OPTIONS'],
+                'login' => ['POST', 'OPTIONS'],               
             ],   
         ];
         
@@ -42,6 +44,56 @@ class UserController extends ActiveController
                 return $model;
             }
         }
+    }
+
+    /**
+      *TODO : add verbs;
+      */
+    public function actionSignup()
+    {
+            $model = new SignupForm();
+            if ($model->load(Yii::$app->getRequest()->getBodyParams(), '')) {
+                if ($user = $model->signup()) {                   
+                        print_r(json_encode(array('message'=>'ok')));
+                }else{
+                        $error = $model->getErrors();
+                        print_r(json_encode($error));
+                }
+            }else{
+                print_r(json_encode(array('message'=>'数据请求失败')));
+            }
+    }
+
+    public function actionSignin()
+    {
+        if (\Yii::$app->getRequest()->getMethod() === 'OPTIONS') {
+            \Yii::$app->getResponse()->getHeaders()->set('Allow', 'POST');
+        } else {
+             $model = new LoginForm();
+
+            if ($model->load(\Yii::$app->getRequest()->getBodyParams(), '') && $model->login()) {
+                $auth = \Yii::$app->user->identity->getAuthKey();
+                $user = \common\models\User::find()->where(['auth_key' => $auth])->one();
+                if($user){
+                        return array('username'=>$user->username,'email'=>$user->email,'user_id'=>$user->id,'auth'=>$user->auth_key);
+                }
+            } else {
+                $model->validate();
+                $errors =  $model->getErrors();
+                return $errors;
+
+            }
+        }
+    }
+
+    public function actionEdit()
+    {
+            $p= \Yii::$app->getRequest()->getBodyParams();
+            $auth = $p['auth'];
+            $user = \common\models\User::find()->where(['auth_key' =>$auth])->one();
+            $user->username = $p['username'];
+            $user->email = $p['email'];
+            return $user->save();            
     }
 
     public function actionIndex()
