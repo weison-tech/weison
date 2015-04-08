7b80ab1d95e0c09f2990685465d974a9216ceb60
xiaomalover <xiaomalover@gmail.com>
2015-04-08 15:39:45 +0800

    update

--- common/config/main.php
+++ common/config/main.php
@@ -1,12 +1,39 @@
 <?php
 return [
+	'language' => 'zh-CN',
 	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
 	'components' => [
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
 		],
-	],
 
+		'authClientCollection' => [
+			    'class' => yii\authclient\Collection::className(),
+			    'clients' => [
+			        'facebook' => [
+			            'class'        => 'dektrium\user\clients\Facebook',
+			            'clientId'     => 'CLIENT_ID',
+			            'clientSecret' => 'CLIENT_SECRET',
+			        ],
+			        'twitter' => [
+			            'class'          => 'dektrium\user\clients\Twitter',
+			            'consumerKey'    => 'CONSUMER_KEY',
+			            'consumerSecret' => 'CONSUMER_SECRET',
+			        ],
+			        'google' => [
+			            'class'        => 'dektrium\user\clients\Google',
+			            'clientId'     => 'CLIENT_ID',
+			            'clientSecret' => 'CLIENT_SECRET',
+			        ],
+			        'github' => [
+				'class'        => 'dektrium\user\clients\GitHub',
+				'clientId'     => 'CLIENT_ID',
+				'clientSecret' => 'CLIENT_SECRET',
+			         ],
+			],
+		],
+	],
+	
 	'modules' => [
 		'user' => [
 			'class' => 'dektrium\user\Module',
