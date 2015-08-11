4b5cb1e640bb0b886a3b9cc8aa89a2448ee6c7a6
xiaomalover <xiaomalover@gmail.com>
2015-08-11 13:33:10 +0800

    update

--- common/config/params.php
+++ common/config/params.php
@@ -4,5 +4,9 @@ return [
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
     'imgUrl' => 'http://img.chayuancun.com',
-    'imgFolder' => '/home/wwwroot/weison/uploads'
+    'imgFolder' => '/home/wwwroot/weison/uploads',
+    'maxRateLimit'=>1,
+    'perRateLimit'=>30,
+    'maxGetRateLimit'=>2,
+    'perGetRateLimit'=>1,
 ];
