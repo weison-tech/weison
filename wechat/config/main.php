cb3905e9c609df57d815ad407217fbc0f39c027e
xiaomalover <xiaomalover@gmail.com>
2015-08-24 23:28:46 +0800

    update

--- wechat/config/main.php
+++ wechat/config/main.php
@@ -31,6 +31,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'wechat' => [
+            'class' => 'callmez\wechat\sdk\Wechat',
+            'appId' => '微信公众平台中的appid',
+            'appSecret' => '微信公众平台中的secret',
+            'token' => '微信服务器对接您的服务器验证token'
+        ],
     ],
     'params' => $params,
 ];
